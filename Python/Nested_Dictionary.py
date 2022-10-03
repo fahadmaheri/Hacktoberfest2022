4b09404b6d05d1bf3d37cb4b829009edb0a97e6d
Fahad Umar Maheri <58872156+fahadmaheri@users.noreply.github.com>
2022-10-03 16:41:42 +0500

    Create Nested_Dictionary.py
    Nested Dictionary in Python

---
+++ Python/Nested_Dictionary.py
@@ -0,0 +1,20 @@
+#Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+
+child1 = {
+  "name" : "Fahad",
+  "year" : 2022
+}
+child2 = {
+  "name" : "Muzamil",
+  "year" : 2021
+}
+child3 = {
+  "name" : "Aasif",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
